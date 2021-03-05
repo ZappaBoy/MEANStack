93f78db4e18d0f34dee6d14999b006b6cd7837fc
zappaboy <federico_zappone@hotmail.it>
2021-03-05 12:06:14 +0100

    Fixed docker node issues CORS

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 8888
 
 const FRONTEND_IP = process.env.FRONTEND_IP || 'localhost'
 const FRONTEND_PORT = process.env.FRONTEND_PORT || '4200'
-const FRONTEND_ORIGIN = '*' //'http://' + FRONTEND_IP + ':' + FRONTEND_PORT
+const FRONTEND_ORIGIN = 'http://' + FRONTEND_IP + ':' + FRONTEND_PORT
 
 let corsOptions = {
     allowedHeaders: ['Content-Type'],
